695d3277d168dc838f580c372c22a4e4b04ca279
giant-paw <giantpawbusiness@gmail.com>
2023-07-02 09:24:49 +0700

    case 3 dan 4

--- binarysearchtree/binarysearchtree.cpp
+++ binarysearchtree/binarysearchtree.cpp
@@ -147,6 +147,18 @@ int main()
 			obj.inorder(obj.ROOT);
 			break;
 		}
+
+		case '3': {
+			obj.preorder(obj.ROOT);
+			break;
+		}
+
+		case '4': {
+			obj.postorder(obj.ROOT);
+			break;
+		}
+
+
 		}
 	}
 }
